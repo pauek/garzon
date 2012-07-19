e4188a19c0ca37ce0953160e8540dcd403c66449
Pau Fernández <pau.fernandez@upc.edu>
2012-07-19 13:23:04 +0200

    launch.sh: para lanzar la VM

---
+++ launch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+tceimg=$1
+if [ -z $tceimg ]; then
+   tceimg='tce-good.img'
+fi
+shift
+
+kvm -kernel vmlinuz \
+    -initrd initrd.gz \
+    -append "tce=vda kmap=qwerty/es vga=788" \
+    -drive file=${tceimg},if=virtio \
+    -drive file=shared.img,if=virtio \
+    -m 512M \
+    $*
