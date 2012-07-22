954ce4167febd83849c1267dab35f28694963ff0
Pau Fernández <pau.fernandez@upc.edu>
2012-07-22 16:04:14 +0200

    Mejoro bootstrap
    1. No descargar core.gz y vmlinuz todo el rato.
    2. Introducir el usuario garzon (/home/garzon + /etc/passwd):
       este usuario tiene menos privilegios que 'tc', no es 'sudoer'

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,32 +27,53 @@ function create_shared_disk() {
 }
 
 function remaster_initrd() {
-    go install garzon/driver
+# Unpack
     sudo rm -rf initrd
     mkdir -p initrd
     pushd initrd
-
-    # unpack
     zcat ../core.gz | sudo cpio -i -H newc -d 
 
-    # introduce modifications 
+# Introduce modifications 
+
+    # create mountpoint for iso
     sudo mkdir -p mnt/cdrom
+
+    # copy driver program
+    go install garzon/driver
     sudo cp $(which driver) usr/bin/driver
+
+    # enable root terminal on serial port
     sudo sh -c "cat >> etc/inittab" <<EOF
 # garzon
 ttyS0::once:/bin/sh
 EOF
+
+    # permit execution of script from 'vda' partition
     sudo sh -c "cat >> opt/bootlocal.sh" <<EOF
 # call init script in apps partition
 source /mnt/vda/tce/bootlocal.sh 
 EOF
 
-    # repack
+    # create user 'garzon'
+    sudo sh -c "cat >> etc/passwd" <<EOF
+garzon:x:5000:5000:Garzon,,,:/home/garzon:/bin/sh
+EOF
+    sudo sh -c "cat >> etc/group" <<EOF
+garzon:x:5000:
+EOF
+    sudo mkdir -p home/garzon
+    sudo chown 5000:5000 home/garzon
+    sudo chmod 700 home/garzon
+   
+# Repack
     sudo find | sudo cpio -o -H newc | gzip -2 > ../initrd.gz
     popd
     sudo rm -rf initrd
 }
 
-download_kernel_and_initrd
+read -p "Download kernel and initrd? (y/n): "
+if [ $REPLY = "y" -o $REPLY = "Y" ]; then
+   download_kernel_and_initrd
+fi
 create_shared_disk
 remaster_initrd
