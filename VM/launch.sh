010f4dd1ff6c00be4d1fa60b1534662509310a9a
Pau Fernández <pau.fernandez@upc.edu>
2012-08-15 17:26:03 +0200

    VM: launch.sh utiliza los mismos parámetros que el worker

--- VM/launch.sh
+++ VM/launch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-kvm -kernel vmlinuz \
-    -initrd initrd.gz \
+kvm -kernel VM/vmlinuz \
+    -initrd VM/initrd.gz \
     -append "tce=vda kmap=qwerty/es vga=788 nodhcp" \
-    -drive file=tce.img,if=virtio \
+    -drive file=VM/tce.img,if=virtio \
     -serial stdio \
     -net none \
     $*
