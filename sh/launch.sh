841bee32cc40683dfaa475297a18153cbd2dd2be
Pau Fernández <pau.fernandez@upc.edu>
2012-09-20 12:26:19 +0200

    grz-vm: Acabada la conversión de los scripts anteriores

--- sh/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-kvm -kernel VM/vmlinuz \
-    -initrd VM/initrd.gz \
-    -append "tce=vda kmap=qwerty/es vga=788 nodhcp" \
-    -drive file=VM/tce.img,if=virtio \
-    -serial stdio \
-    -net none \
-    $*
